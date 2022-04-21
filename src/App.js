3e06e453d250f5d9b710ae0c008a3bd3fe46bca0
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-21 08:49:26 +0200

    Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
